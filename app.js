c2362e24e710c2e96800ea3f3a005ca60827b18f
lav-joshi <lcs2019022@iiitl.ac.in>
2020-04-10 03:56:56 +0530

    weather-app

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const ejs = require('ejs');
+const bodyParser = require("body-parser");
+const request=require("request")
+const app = express();
+const key=require("./config/keys")
+app.set('view engine', 'ejs');
+const port=process.env.PORT || 3000;
+
+app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const url="http://api.weatherstack.com/current?access_key="+"4d643754bd86f209879a2790558581df"+"&query="
+app.get("/", function(req, res) {
+    res.render("index");
+});
+
+app.get("/weather",(req,res)=>{
+    var x=req.query.address;
+    request(url+x,(error,response,body)=>{
+       var data=JSON.parse(body);
+       if(data.success==false){
+          res.send({
+              error:"error"
+          })
+       }else{
+        res.send({
+           name:data.location.name ,
+           localtime:data.location.localtime,
+           temperature:data.current.temperature,
+           weather_descriptions:data.current.weather_descriptions[0],
+           weather_icons:data.current.weather_icons
+       });
+      }
+    })
+})
+
+app.listen(port, function() {
+    console.log("server is listening!!!");
+});
---
+++ public/js/styles.js
@@ -0,0 +1,43 @@
+const search=document.querySelector("input");
+const WeatherForm=document.querySelector("form");
+const MessageOne=document.querySelector("#message-1");
+const MessageTwo=document.querySelector("#message-2");
+const MessageThree=document.querySelector("#message-3");
+const MessageFour=document.querySelector("#message-4");
+const MessageFive=document.querySelector("#message-5");
+
+const loader=document.querySelector(".hello");
+
+
+WeatherForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    const location=search.value;
+
+    loader.classList.add("loader");
+    MessageOne.innerHTML="";
+    MessageTwo.innerHTML="";
+    MessageThree.innerHTML="";
+    MessageFour.innerHTML="";
+    MessageFive.setAttribute("src","");
+    // fetch("http://localhost:3000/weather?address="+location).then(function(response){
+
+    fetch("/weather?address="+location).then(function(response){
+        response.json().then(function(data){
+           if(data.error){
+               MessageOne.innerHTML="Something went Wrong";
+               loader.classList.remove("loader");
+           }else{
+            loader.classList.remove("loader");
+            MessageOne.innerHTML="City Name : "+data.name;
+            MessageTwo.innerHTML="Local Time: "+data.localtime;
+            MessageThree.innerHTML="Temperature: "+data.temperature+" degree Celcius";
+            MessageFour.innerHTML="Weather Description: "+data.weather_descriptions;
+            MessageFive.setAttribute("src",data.weather_icons);
+
+
+            
+
+           }
+        })
+    })
+});
